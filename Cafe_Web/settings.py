6feb15fd37ce78b3c82d89497f15d28c7b095890
wmg718 <walex.7029@gmail.com>
2025-11-29 11:18:14 -0500

    Configured MEDIA settings for Railway storage

--- Cafe_Web/settings.py
+++ Cafe_Web/settings.py
@@ -152,5 +152,6 @@ STATICFILES_DIRS = []
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-MEDIA_URL = '/media/'
-MEDIA_ROOT = '/data/media'
+
+MEDIA_URL = os.getenv("MEDIA_URL", "/media/")
+MEDIA_ROOT = os.getenv("MEDIA_ROOT", os.path.join(BASE_DIR, "media"))
